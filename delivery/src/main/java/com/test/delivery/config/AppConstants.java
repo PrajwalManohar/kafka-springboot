ad4064deb3dcc09e1bfa974248df17569f20513b
PrajwalManohar <mprajwal1998@gmail.com>
2023-12-14 18:47:46 +0530

    Onboarding Kafka to springboot

---
+++ delivery/src/main/java/com/test/delivery/config/AppConstants.java
@@ -0,0 +1,6 @@
+package com.test.delivery.config;
+
+public class AppConstants {
+    public static final String LOCATION_TOPIC_NAME = "location-update-topic";
+    public static final String LOCATION_TOPIC_NAME1 = "location-update-test";
+}
